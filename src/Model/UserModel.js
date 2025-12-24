6f0f5e4fb05302becd0b82c99bff70abdedc3f85
ronak bendwal <ronakbendwal7880@gmail.com>
2025-12-24 08:02:46 +0530

    Write code for Token Generation Or for Passward Hashing and Checking

--- src/Model/UserModel.js
+++ src/Model/UserModel.js
@@ -1,5 +1,6 @@
 import mongoose,{Schema,model} from "mongoose";
-
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 const UserSchema=new Schema({
   Username:{
     type:String,
@@ -26,5 +27,47 @@ const UserSchema=new Schema({
 },{timestamps:true})
 
 
+
+//it is an middleware hash the bassward before save and safe the passward from unuseal hashing
+UserSchema.pre("save",async function(next){
+  if(!this.isModified(passward)) return next();
+
+  this.passward=await bcrypt.hash(this.passward,10)
+  next();
+})
+
+//it is an middleware user to chect the passward is correct or not
+UserSchema.method.IsPasswardCorrect=async function(passward){
+  return await bcrypt.compare(passward,this.passward)
+}
+
+
+//now we're gonna create custom method for the generation of access and refresh token
+UserSchema.method.generateAccessToken=function(){
+jwt.sign(
+  {
+    username:this.Username,
+    fullname:this.FullName,
+    email:this.Email,
+    id:this._id
+  },
+  process.env.REFRESH_TOKEN_SECRET,
+  {
+    expiresIn:"1D"
+  },
+)
+}
+//refresh token generated
+UserSchema.method.generateRefreshToken=function(){
+  jwt.sign(
+    {
+      id:this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn:"10D"
+    }
+  )
+}
 export const USER=model("USER",UserSchema)
 
