85041c41d953a66e23597bf6e4051b4831487e87
ronak bendwal <ronakbendwal7880@gmail.com>
2025-12-31 22:54:52 +0530

    Install dev dependency : lucide-react

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import './App.css'
-
+import HeaderComponent from './Components/Header.Component'
 function App() {
   return (
     <>
+    <HeaderComponent/>
     </>
   )
 }
