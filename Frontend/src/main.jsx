b3c87b2dc39161f0e970d957eaadebcabe49e8b7
ronak bendwal <ronakbendwal7880@gmail.com>
2026-01-06 10:25:18 +0530

    Provide store to all component

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import {createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import MainLayout from './Outlet/MainLayout.jsx';
 import { LoginForm,SignupForm,ItemsPreviewSection} from './Components/index.js';
 import { RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './redux/Store/FeatureStore.js';
 import App from './App.jsx';
 
 
@@ -22,9 +23,11 @@ const router=createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <RouterProvider router={router}/>
-      {/* <BrowserRouter>
-      <App/>
-      </BrowserRouter> */}
+      <Provider store={store}>
+        <RouterProvider router={router}/>
+      </Provider>
   </StrictMode>
 )
+    {/* <BrowserRouter>
+      <App/>
+    </BrowserRouter> */}
