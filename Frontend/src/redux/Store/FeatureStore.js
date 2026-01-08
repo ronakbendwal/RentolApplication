fd89fbdd9b3a9d1b28bb22959711c31d00b1775b
ronak bendwal <ronakbendwal7880@gmail.com>
2026-01-08 08:27:28 +0530

    Congifure auth reducer

--- Frontend/src/redux/Store/FeatureStore.js
+++ Frontend/src/redux/Store/FeatureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import LogoutReducer from '../Feature/LogoutUi.js'
 import ThemeReducer from '../Feature/Theme.js'
+import AuthReducer from '../Feature/Auth.js'
 const store=configureStore({
   reducer:{
     logoutState:LogoutReducer,
     theme:ThemeReducer,
+    auth:AuthReducer
   }
 });
 
