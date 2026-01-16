999c95e0eab83b4af1b3f4108dfe6ac905d54848
ronak bendwal <ronakbendwal7880@gmail.com>
2026-01-16 10:44:50 +0530

    Make separate the Address field

---
+++ Frontend/src/Form/Utils/Address.jsx
@@ -0,0 +1,32 @@
+import React, { forwardRef, useId } from 'react'
+const Address=forwardRef(function Address({
+  innercolor,
+  className,
+  ...props
+},referance) {
+  const id =useId();
+  return (
+   <div className="space-y-2">
+    <label 
+    className="text-sm font-bold text-gray-700 ml-1 flex items-center gap-1.5"
+    >Full Pickup Address
+    </label>
+      <div 
+      className="relative">
+        <Map 
+        className="absolute left-5 top-4 text-gray-400" 
+        size={18}/>
+        <input 
+        type="text" 
+        placeholder="House No, Street Name, Landmark..." 
+        className={`w-full pl-12 pr-5 py-4 bg-gray-50 border border-gray-100 rounded-2xl outline-none focus:border-${innercolor}-500 focus:bg-white transition-all font-medium ${className}`}
+        {...props}
+        id={id}
+        ref={referance}
+        />
+      </div>
+   </div>
+  )
+})
+
+export default Address
