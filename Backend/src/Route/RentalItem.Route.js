507815730b50cf866826fd7a6cc53a3dc417b425
ronak bendwal <ronakbendwal7880@gmail.com>
2026-01-31 10:12:56 +0530

    Provide routing to the rateitem controller

--- Backend/src/Route/RentalItem.Route.js
+++ Backend/src/Route/RentalItem.Route.js
@@ -8,18 +8,22 @@ import {
   GetYouritem,
   DeleteItemImage,
   UploadMoreImage,
-  UpdateItem
+  UpdateItem,
+  RateItem
  } from "../Controller/RentalItem.Controller.js";
+import { verify } from "jsonwebtoken";
 const RentItemRouter=Router();
 
-RentItemRouter.delete("/deleteimage/:itemId/:imageId",VerifyUser,DeleteItemImage);
-RentItemRouter.post("/updateitem/:itemId",VerifyUser,UpdateItem);
-RentItemRouter.delete("/deleteitem/:itemId",VerifyUser,DeleteItem);
 RentItemRouter.post("/rentoutitem",VerifyUser,Multer.array("images",6),rentOutItem);
+RentItemRouter.post("/updateitem/:itemId",VerifyUser,UpdateItem);
+RentItemRouter.post("/rate-item/:itemid",VerifyUser,RateItem)
 RentItemRouter.get("/getitem/:itemId",VerifyUser,GetItem);
 RentItemRouter.get("/getallitem",VerifyUser,GetAllItem);
 RentItemRouter.get("/getyouritem",VerifyUser,GetYouritem);
 RentItemRouter.patch("/uploadimages/:itemId",VerifyUser,Multer.array("images",6),UploadMoreImage);
+RentItemRouter.delete("/deleteitem/:itemId",VerifyUser,DeleteItem);
+RentItemRouter.delete("/deleteimage/:itemId/:imageId",VerifyUser,DeleteItemImage);
+
 
 
 export default RentItemRouter;
